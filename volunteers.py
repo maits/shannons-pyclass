35a3f7f391520ba0b58dd12c13ccfa19a1e31e4d
Maite Fernandez <maite.fern@gmail.com>
2014-02-23 19:43:00 -0500

    Create volunteers.py

---
+++ volunteers.py
@@ -0,0 +1,15 @@
+volunteers_goal = 100
+
+current_volunteers = 100
+
+# Ask: Is current_volunteers less than, equal to, or greater,
+# than volunteers_goal?
+
+if current_volunteers < volunteers_goal:
+	print "We are not there yet"
+
+elif current_volunteers == volunteers_goal:
+	print "Yaay! We made it!"
+
+else: 
+	print "We surpassed our goal! We're awesome!" 
