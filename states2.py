09b3e4d64cb39d330592f4b9d1ffa85ae943468d
Maite Fernandez <maite.fern@gmail.com>
2014-04-01 23:43:50 -0400

    Create states2.py

---
+++ states2.py
@@ -0,0 +1,45 @@
+with open("states.txt", "r") as states_file:
+	states = states_file.read().split("\n")
+
+	print "<select name='States'>"
+
+	for index, state in enumerate(states):
+		states[index] = state.split("\t")
+
+	for state in states:
+		print "\t <option value='{0}'>{1}</option>".format(state[0], state[1])
+
+
+print "</select>"
+
+######
+
+with open("states.txt", "r") as states_file:
+	states = states_file.read().split("\n")
+
+#	print "<select name='States'>"
+
+	for index, state in enumerate(states):
+		states[index] = state.split("\t")
+
+	with open("states.html", "w") as new_file:		
+		new_file.write("<select name='States'>")
+
+
+		for state in states:
+			new_file.write("\t <option value='{0}'>{1}</option>".format(state[0], state[1]))
+
+
+		new_file.write("</select>")
+
+
+
+
+
+
+
+
+
+
+
+ 
