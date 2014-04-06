783a86ab3a60ec022cb8a33f74810dd15ef59f57
Maite Fernandez <maite.fern@gmail.com>
2014-04-06 16:57:22 -0400

    Create lesson3-contacts-goal1.py

---
+++ lesson3-contacts-goal1.py
@@ -0,0 +1,25 @@
+# Challenge Level: Beginner
+
+# NOTE: Please don't use anyone's *real* contact information during these exercises, especially if you're putting it up on Github!
+
+# Background: You have a dictionary with people's contact information.  You want to display that information as an HTML table.
+
+contacts = {
+    'Shannon': {'phone': '202-555-1234', 'twitter': '@svt827', 'github': '@shannonturner' }, 
+    'Beyonce': {'phone': '303-404-9876', 'twitter': '@beyonce', 'github': '@bey'},
+    'Tegan and Sara': {'phone': '301-777-3313', 'twitter': '@teganandsara', 'github': '@heartthrob'}
+}
+
+# Goal 1: Loop through that dictionary to print out everyone's contact information.
+
+# Sample output:
+
+# Shannon's contact information is:
+#   Phone: 202-555-1234
+#   Twitter: @svt827
+#   Github: @shannonturner 
+
+# Beyonce's contact information is:
+#   Phone: 303-404-9876
+#   Twitter: @beyonce
+#   Github: @bey
