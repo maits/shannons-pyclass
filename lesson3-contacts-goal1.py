d8e102e5d94a09bf87375863db61a61bda5f91c1
Maite Fernandez <maite.fern@gmail.com>
2014-04-11 07:54:01 -0400

    Update lesson3-contacts-goal1.py

--- lesson3-contacts-goal1.py
+++ lesson3-contacts-goal1.py
@@ -29,3 +29,55 @@ contacts = {
 for contact,values in contacts.items():
 	print "\n" "\t" "{0}'s contact information is:" "\n" "\t" .format(contact) + "Phone:" + ' ' + values.values()[0] + "\n" "\t"  "Twitter:" + ' ' + values.values()[1] + "\n" "\t"  "GitHub:" + ' ' + values.values()[2] 
 
+# Goal 2:  Display that information as an HTML table.
+
+# Sample output:
+
+# <table border="1">
+# <tr>
+# <td colspan="2"> Shannon </td>
+# </tr>
+# <tr>
+# <td> Phone: 202-555-1234 </td>
+# <td> Twitter: @svt827 </td>
+# <td> Github: @shannonturner </td>
+# </tr>
+# </table>
+
+for contact,values in contacts.items():
+		print "\n"
+		print '<table border="1">'
+		print '<tr>'
+		print '<td colspan="2">'  
+		print contact
+		print '</td>' 
+		print '</tr>' 
+		print '<tr>' 
+		print '<td>'"Phone:" + ' ' + values.values()[0] 
+		print '</td>'
+		print '<td>'"Twitter:" + ' ' + values.values()[1] 
+		print '</td>' 
+		print '<td>'"GitHub:" + ' ' + values.values()[2]
+		print '</td>' 
+		print '</tr>'
+		print '</table>'
+
+#I found a better way of doing this in ElizabethAB's github
+
+html_string = ""
+
+for contact, info in contacts.items():
+	html_string += """<table border="1">
+<tr>
+<td colspan="2"> {0} </td>
+</tr>
+<tr>
+<td> Phone: {1} </td>
+<td> Twitter: {2} </td>
+<td> Github: {3} </td>
+</tr>
+</table>
+	""".format(contact, info.get('phone'), info.get('twitter'), info.get('github'))
+	print html_string
+
+
