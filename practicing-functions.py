8f79dd5fc6de50d61e725d0a75cb0f4984626d81
Maite Fernandez <maite.fern@gmail.com>
2014-04-16 00:34:04 -0400

    Create practicing-functions.py

---
+++ practicing-functions.py
@@ -0,0 +1,24 @@
+#lesson 4 - slide 16
+
+#Create a function that will return a string containing 'Hello, <name>!' 
+
+#So when you call the function like this: 
+
+#print greeting('Shannon') 
+
+
+#This should happen in response: 
+
+#Hello, Shannon!
+
+name = "Shannon"
+greeting = "Hello, {0}".format(name)
+
+print greeting
+
+#writing the same thing as a function 
+
+def greeting(name):
+    name = "Shannon"
+    print "Hello again,", name 
+greeting("Shannon")
