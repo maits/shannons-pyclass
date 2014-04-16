b4bcf26885e16a4053a9287ef893b9d91cb18674
Maite Fernandez <maite.fern@gmail.com>
2014-04-16 00:34:28 -0400

    Update practicing-functions.py

--- practicing-functions.py
+++ practicing-functions.py
@@ -1,12 +1,8 @@
 #lesson 4 - slide 16
 
 #Create a function that will return a string containing 'Hello, <name>!' 
-
 #So when you call the function like this: 
-
 #print greeting('Shannon') 
-
-
 #This should happen in response: 
 
 #Hello, Shannon!
