b738de5ac1adb0d614db19ad3e6020075b6a0a36
Maite Fernandez <maite.fern@gmail.com>
2014-04-16 23:26:28 -0400

    Update lesson4-converting-code-into-functions.py

--- lesson4-converting-code-into-functions.py
+++ lesson4-converting-code-into-functions.py
@@ -1,5 +1,9 @@
+#from lesson 3
+
 with open("states.txt", "r") as states_file:
   states = states_file.read() 
+
+#converting it into a function
 
 def textfile_to_string(filename):
   with open (filename, "r") as text_file:
